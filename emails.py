f43f5ad1077c8956279ba008092058d0dd20f0fb
Guillermo Gutierrez <guillermo20@gmail.com>
2017-11-03 13:42:00 -0500

    done with the script

---
+++ emails.py
@@ -0,0 +1,5 @@
+
+emails_list = {'email1@email.com', 'email2@email.com'}
+
+sender_email = 'email12345@email.com'
+sender_password = 'password123'
--- notifier.py
+++ notifier.py
@@ -1,10 +1,14 @@
 import urllib2
 import time
+import emails
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 
 def internet_on():
     try:
-        # as of november the third saime is throwing a code 503 to let the user
+        # as of november the third, saime is throwing code 503 to let the user
         # know that the site is down
         urllib2.urlopen('https://tramites.saime.gob.ve', timeout=60)
         return True
@@ -12,14 +16,27 @@ def internet_on():
         return False
 
 
+def send_emails():
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(emails.sender_email, emails.sender_password)
+    msg = MIMEMultipart()
+    # the subject is setted just to add more context
+    msg['Subject'] = "Saime status service"
+    body = "Saime is active!"
+    msg.attach(MIMEText(body, 'plain'))
+    for e in emails.emails_list:
+        server.sendmail(emails.sender_email, e, msg.as_string())
+    server.quit()
+
+
 timer = 30
 while True:
     if(internet_on()):
         print("found url")
-        timer = 3600
-        # TODO: read a property to find the emails to send the notification
-        # TODO: and then call the function that sends the actual email
+        timer = 1800  # half an hour
+        send_emails()
     else:
         print("error finding the url")
-        timer = 30
+        timer = 30  # 30 seconds
     time.sleep(timer)
